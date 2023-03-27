5cee647a1985d94bef884abe79d0eca506e692a6
hugo <hugo.ahmed@enti.cat>
2023-03-28 01:11:55 +0200

    Alpha

--- Assets/Scripts/Enemy/RandomSpawn.cs
+++ Assets/Scripts/Enemy/RandomSpawn.cs
@@ -6,8 +6,8 @@ public class RandomSpawn : MonoBehaviour
 {
     public Transform[] SpawnPoint;
     public GameObject[] enemy;
-    public int time = 3000;
-    public int Retime = 3000;
+    public float timer = 3000;
+    public float Retime = 3000;
     private bool cantRespawn = false;
     // Start is called before the first frame update
     void Start()
@@ -20,14 +20,15 @@ public class RandomSpawn : MonoBehaviour
     {
         if(cantRespawn == false)
         {
-        time -=1;
-            if (time == 0)
+        timer -= Time.deltaTime;
+
+            if (timer <= 0)
             {
                 int randEnemy = Random.Range(0, enemy.Length);
                 int randSpawn = Random.Range(0, SpawnPoint.Length);
 
                 Instantiate(enemy[0], SpawnPoint[randSpawn].position, transform.rotation);
-                time = Retime;
+                timer = Retime;
             }
         }
     }
